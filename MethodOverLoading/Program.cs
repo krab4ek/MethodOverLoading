17159a8a0be25f1f8c60517b8685795b62e75596
Covali Andrei <krab4ek@gmail.com>
2022-06-23 18:09:52 +0300

    Add project files.

---
+++ MethodOverLoading/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Перегрузка методов
+Console.WriteLine("***** Fun with Method Overloading *****");
+Console.WriteLine($"Summ: \t{Calculator.Add(10, 10)}");
+Console.WriteLine($"Summ: \t{Calculator.Add(4.6, 7.9)}");
+Console.WriteLine($"Summ: \t{Calculator.Add(900_000_000, 900_000_000_000)}");
+
+
+#endregion
+public class Calculator
+{
+    public static int Add(int x, int y)
+    { return x + y; }
+
+    public static long Add(long x, long y)
+    { return x + y; }
+
+    public static double Add(double x, double y)
+    { return x + y; }
+}
